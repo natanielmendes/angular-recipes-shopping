af864344bc0391366823560bdf093e7bc9694d9c
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-04-28 20:09:14 -0300

    Recipe component with no communication between list and details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test.', 'https://p0.pikrepo.com/preview/1002/340/chocolate-cake-chocolate-cake-recipe.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test.', 'https://p0.pikrepo.com/preview/1002/340/chocolate-cake-chocolate-cake-recipe.jpg')
+  ];
 
   constructor() { }
 
